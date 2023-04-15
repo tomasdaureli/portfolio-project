c53a16d155cd80c267fdbc9088e47a29b21c52c4
Tomas <tomasdaureli@gmail.com>
2023-04-15 18:01:29 -0300

    Creacion Base de datos + Hide secret key

--- project/settings.py
+++ project/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-26_m$@+z^jbjr(%nmy#kw=5nvxl%-dg$r&-qyb=)mk*fyqzl9('
+with open(os.path.join(BASE_DIR, 'secret.txt')) as f:
+    SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
