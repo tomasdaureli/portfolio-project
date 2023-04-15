8f73ab56ee261def87a366f39f82c0dfff3b8744
Tomas <tomasdaureli@gmail.com>
2023-04-15 18:10:24 -0300

    Creacion models + Carga templates + Creacion vistas

--- app/models.py
+++ app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.db.models.fields import CharField, URLField, EmailField
 
 # Create your models here.
+class Project(models.Model):
+    title = CharField(max_length=100)
+    description = CharField(max_length=250)
+    link = URLField(blank=True)
+
+class User(models.Model):
+    name = CharField(max_length=100)
+    job = CharField(max_length=100)
+    bio = CharField(max_length=1000)
+    knowledge = CharField(max_length=1000)
+    email = EmailField(max_length=255)
+    github = URLField(blank=True)
+    linkedin = URLField(blank=True)
--- app/views.py
+++ app/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import User, Project
 
 # Create your views here.
+def index(request):
+        projects = Project.objects.all()
+        user = User.objects.get(id=1)
+        return render(request, 'home.html', {
+                "projects": projects,
+                "user": user
+        })
