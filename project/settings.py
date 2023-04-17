4f6ea673ccef2ed4aace600a72b35654752dc8b9
Tomas <tomasdaureli@gmail.com>
2023-04-17 20:31:44 -0300

    allow host

--- project/settings.py
+++ project/settings.py
@@ -26,7 +26,7 @@ with open(os.path.join(BASE_DIR, 'secret.txt')) as f:
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
